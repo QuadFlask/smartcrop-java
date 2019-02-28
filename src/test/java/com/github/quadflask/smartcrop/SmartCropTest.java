9cd37c6ba00a84c86751c2f3f9765156d7ab461a
Damien Papworth <damien_papworth@hotmail.com>
2019-02-28 18:35:02 +0000

    Added support for face detection.

--- src/test/java/com/github/quadflask/smartcrop/SmartCropTest.java
+++ src/test/java/com/github/quadflask/smartcrop/SmartCropTest.java
@@ -1,13 +1,28 @@
 package com.github.quadflask.smartcrop;
 
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.opencv_java;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 import javax.imageio.ImageIO;
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.opencv.objdetect.Objdetect.CASCADE_SCALE_IMAGE;
 
 /**
  * Created by flask on 2015. 10. 27..
@@ -18,35 +33,56 @@ public class SmartCropTest {
 	static String debugPath = "src/test/resources/debug";
 	static String resultPath = "src/test/resources/result";
 
+	private static CascadeClassifier faceCascade;
+
+	@BeforeClass
+	public static void setUp() {
+		Loader.load(opencv_java.class);
+
+		faceCascade = new CascadeClassifier();
+		faceCascade.load("src/test/resources/haarcascade_frontalface_alt.xml");
+	}
+
 	@Test
 	public void test() throws Exception {
 		Arrays.stream(new File(samplePath).listFiles(pathname -> pathname.getName().endsWith(".jpg"))).
 				forEach(file -> {
 					try {
+						System.out.println("Processing " + file.getName());
 						BufferedImage input = ImageIO.read(file);
+						List<Boost> faces = detectFaces(input);
+						System.out.println("Detected " + faces.size() + " faces");
 						String baseName = baseName(file.getName());
 
 						Options options = new Options();
-						SmartCrop smartCrop = SmartCrop.analyze(options, input);
+						SmartCrop smartCrop = SmartCrop.analyze(options.boost(faces), input);
 
-						createCrop(input, smartCrop, options.aspect(1.0f), baseName + "_11");
-						createCrop(input, smartCrop, options.aspect(1.33f), baseName + "_1133");
-						createCrop(input, smartCrop, options.aspect(1.91f), baseName + "_1191");
+						createCrop(input, smartCrop, options.aspect(1.0f), baseName + "_11", faces);
+						createCrop(input, smartCrop, options.aspect(1.33f), baseName + "_1133", faces);
+						createCrop(input, smartCrop, options.aspect(1.91f), baseName + "_1191", faces);
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
 				});
 	}
 
+
 	private String baseName(String fileName) {
 		int extenstionIndex = fileName.lastIndexOf('.');
 		return fileName.substring(0, extenstionIndex);
 	}
 
-	private void createCrop(BufferedImage input, SmartCrop smartCrop, Options options, String filename) throws Exception {
+	private void createCrop(BufferedImage input, SmartCrop smartCrop, Options options, String filename, List<Boost> boost) throws Exception {
 		CropResult cropResult = smartCrop.generateCrops(options);
 		BufferedImage cropImage = createCrop(input, cropResult.topCrop);
 		ImageIO.write(cropImage, "png", new File(resultPath, filename + ".png"));
+
+		Graphics2D g = (Graphics2D) cropResult.debugImage.getGraphics();
+		g.setColor(Color.WHITE);
+		boost.forEach(b -> {
+			g.drawRect(b.x, b.y, b.width, b.height);
+		});
+		g.dispose();
 		ImageIO.write(cropResult.debugImage, "png", new File(debugPath, filename + ".png"));
 	}
 
@@ -57,5 +93,34 @@ public class SmartCropTest {
 		g.drawImage(input, 0, 0, crop.width, crop.height, crop.x, crop.y, crop.x + crop.width, crop.y + crop.height, null);
 		g.dispose();
 		return image;
+	}
+
+	private List<Boost> detectFaces(BufferedImage input) {
+		byte[] data = ((DataBufferByte) input.getRaster().getDataBuffer()).getData();
+		Mat frame = new Mat(input.getHeight(), input.getWidth(), CvType.CV_8UC3);
+		frame.put(0, 0, data);
+
+		// convert the frame to gray scale
+		Mat grayFrame = new Mat();
+		Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_RGB2GRAY);
+		// equalize the frame histogram to improve the result
+		Imgproc.equalizeHist(grayFrame, grayFrame);
+
+		int maxSize = Math.max(input.getWidth(), input.getHeight());
+		int minFaceSize = (int) (maxSize * 0.05);
+
+		// detect faces
+		MatOfRect faces = new MatOfRect();
+		faceCascade.detectMultiScale(grayFrame, faces, 1.1, 2, CASCADE_SCALE_IMAGE, new Size(minFaceSize, minFaceSize), new Size(maxSize, maxSize));
+
+		return faces.toList().stream().map(face -> {
+			Boost boost = new Boost();
+			boost.x = face.x;
+			boost.y = face.y;
+			boost.width = face.width;
+			boost.height = face.height;
+			boost.weight = 1.0f;
+			return boost;
+		}).collect(Collectors.toList());
 	}
 }
